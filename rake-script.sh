787e51b943e1cdf4a6b205d09afeafa89b7b35e4
Maham Nazir <maham.nazir@confiz.com>
2017-12-05 19:09:56 +0500

    Final Changes Added

--- rake-script.sh
+++ rake-script.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 rake spec
+R=$?
 export CODECLIMATE_REPO_TOKEN="03655c58c1bda4ecc6d3da7a02314b4044068a271b9b0d6d8cc20799d50adc54"
 codeclimate-test-reporter
-R=$?
 if [ $R == 1 ]; then
 	echo "RAKE FAILED";
 	exit 1
